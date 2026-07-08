6eeab002d3ccbf47de14a1f8aa6097f380286463
Yash010111 <122040484+Yash010111@users.noreply.github.com>
2026-07-08 14:21:45 +0530

    Update Sort.java

--- Ultimatum/Sorting/Sort.java
+++ Ultimatum/Sorting/Sort.java
@@ -24,9 +24,36 @@ public class Sort{
             System.out.println(arr[i]);
         }
     }
+    public static void selectionSort(int[] arr){
+        for(int i = 0; i<arr.length-1; i++){
+            int minPos = i;
+            for(int j = i+1; j<arr.length; j++){
+                if(arr[minPos]>arr[j]){
+                    minPos = j;
+                }
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void insertionSort(int[] arr){
+        for(int i = 1; i< arr.length; i++){
+            int curr = arr[i];
+            int prev = i-1;
+            while(prev>= 0 && arr[prev]>curr){
+                arr[prev + 1] = arr[prev];
+                prev --;
+                arr[prev+1] = curr;
+            }
+        }
+    }
+
+
     public static void main(String []args){
     int[] arr = {5,4,1,3,2};
-    bubblesort(arr);
+    selectionSort(arr);
     printArr(arr);
     
 }
