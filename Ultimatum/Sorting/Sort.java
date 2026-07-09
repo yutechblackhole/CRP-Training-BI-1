a128df556c14be5a6309f1c196b478c380074ed6
Yash010111 <122040484+Yash010111@users.noreply.github.com>
2026-07-09 19:02:25 +0530

    Update Sort.java

--- Ultimatum/Sorting/Sort.java
+++ Ultimatum/Sorting/Sort.java
@@ -50,6 +50,28 @@ public class Sort{
         }
     }
 
+    public static void countingSort(int[] arr){
+        int max = Integer.MIN_VALUE;
+        for(int i =0; i<arr.length; i++){
+            max = Math.max(max, arr[i]);
+        }
+
+        int count[] = new int[max +1];
+
+        for(int i =0; i<arr.length; i++){
+            count [arr[i]] ++;
+
+        }
+        int j = 0;
+        for(int  i = 0; i<count.length; i++){
+            while(count[i]>0){
+                arr[j] = i;
+                j++;
+                count[i] --;
+            }
+        }
+    }
+
 
     public static void main(String []args){
     int[] arr = {5,4,1,3,2};
